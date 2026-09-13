773f4a97a233f213679d726c9716fd845a091854
tomahmed420 <tomahmed420@gmail.com>
2026-09-13 06:09:23 +0600

    Wire CanvasForge canvas styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,6 @@
-import React from 'react';import{createRoot}from'react-dom/client';import'./styles.css';import{Canvas}from'./Canvas';createRoot(document.getElementById('root')!).render(<React.StrictMode><Canvas/></React.StrictMode>);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'tldraw/tldraw.css';
+import './styles.css';
+import { Canvas } from './Canvas';
+createRoot(document.getElementById('root')!).render(<React.StrictMode><Canvas /></React.StrictMode>);
